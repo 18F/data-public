a34f45f79d72e0350a7d7b855dd6d1c16e362f66
Mike Bland <michael.bland@gsa.gov>
2014-11-30 12:19:44 -0500

    Initial import

---
+++ import.sh
@@ -0,0 +1,4 @@
+#! /bin/sh -x
+rm team.yml projects.yml
+wget https://raw.githubusercontent.com/18F/18f.gsa.gov/staging/_data/team.yml
+wget https://raw.githubusercontent.com/18F/dashboard/gh-pages/_data/projects.yml
