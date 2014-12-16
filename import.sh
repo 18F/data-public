6caecfa77e9ce1d83f0054f9765bef6436a0a99c
Mike Bland <michael.bland@gsa.gov>
2014-12-16 10:20:24 -0500

    Import project updates; update import.sh

--- import.sh
+++ import.sh
@@ -1,4 +1,5 @@
 #! /bin/sh -x
-rm team.yml projects.yml
+rm team.yml pif_projects.yml projects.yml
 wget https://raw.githubusercontent.com/18F/18f.gsa.gov/staging/_data/team.yml
+wget https://raw.githubusercontent.com/18F/18f.gsa.gov/staging/_data/projects.yml -O pif_projects.yml
 wget https://raw.githubusercontent.com/18F/dashboard/gh-pages/_data/projects.yml
